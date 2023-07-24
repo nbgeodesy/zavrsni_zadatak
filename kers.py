6f108bd503f1f8cb4bcc0bf8442b71ef1e9a9b4c
nbgeodesy <nbgeodesy@gmail.com>
2023-07-24 13:27:11 +0200

    Konacna verzija

--- kers.py
+++ kers.py
@@ -7,9 +7,6 @@ import uuid
 import operat
 
 
-
-
-
 app = Flask(__name__)
 app.secret_key='Kers123*'
 bootstrap = Bootstrap(app)
@@ -39,7 +36,7 @@ def registracija():
                    cur.execute('INSERT INTO User(firstname, lastname, username, email, password) VALUES(%s, %s, %s, %s, %s)', [ime, prezime, username, email, password])
                    mysql.connection.commit()
                    cur.close()
-                   flask.flash('Registracija uspjesna! Ulogujte se!', 'succes')
+                   flask.flash('Registracija uspjesna! Ulogujte se!', 'success')
                    return redirect(url_for('login'))
                else:
                    flask.flash('Email već postoji! Molimo registrujte se sa novom email adresom.', 'danger')
@@ -98,57 +95,199 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/kat_operat')
-def kat_ko():
+@app.route('/kat')
+def kat_operat():
     return render_template('kat_operat.html')
 
+@app.route('/napredna')
+def napredna_pretraga():
+    return render_template('napredna_pretraga.html')
 
+
+######################################POSJEDOVNI LIST###############################################################################
 @app.route('/dodaj_pl/', methods = ['GET', 'POST'])
 def dodaj_pl():      
     id_pl = int(uuid.uuid4())
     br_pl = request.form.get('brpl')
     pl = operat.PosList(id_pl, br_pl)
-    dodaj_pl = operat.PosList.dodaj_pl(pl)
+    dodaj_pl = operat.dodaj_pl(pl)
     return render_template('dodaj_pl.html')
-   
 
+
+@app.route('/pretrazi_pl/', methods = ['GET', 'POST'])
+def pretraga_posjednik():
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        pl = request.form.get('brpl')
+        id_pl = operat.br_id_pl(pl)
+        if pl.isdigit():
+            pretrazi_parcelu = cur.execute("SELECT * FROM PARCELE WHERE broj_pl = %s", [pl])
+            if pretrazi_parcelu > 0:
+                preuzmi_parcelu = cur.fetchall()
+                pretrazi_posjednika= cur.execute("SELECT * FROM POSJEDNIK WHERE POS_LIST_id_pl = %s", [id_pl])
+                preuzmi_posjednika = cur.fetchall()
+                flask.flash('Uspjesna pretraga!', 'success')
+                return render_template('pretrazi_pl.html', rezultat = preuzmi_posjednika, rezultat1 = preuzmi_parcelu, pl = pl)
+            else:
+                flask.flash('Ne postoji PL/LN u bazi!', 'danger')
+                return render_template('pretrazi_pl.html') 
+        else:
+            flask.flash('Unos mora biti broj!', 'danger')
+            return render_template('pretrazi_pl.html')
+
+    else:
+        return render_template('pretrazi_pl.html')
+
+
+
+
+######################################POSJEDNICI###############################################################################
 @app.route('/dodaj_posjednika/', methods = ['GET', 'POST'])
 def dodaj_posjednika():
+    br_pl = request.form.get('pl')
+    jmbg = request.form.get('jmbg')
+    ime = request.form.get('ime')
+    prezime = request.form.get('prezime')
+    vrsta_prava = request.form.get('vrstaprava')
+    obim_prava = request.form.get('obimprava')
+    id_pl_preuzet = operat.br_id_pl(br_pl)
+    posjednik = operat.Posjednici(jmbg, ime, prezime, vrsta_prava, obim_prava, id_pl_preuzet)
+    dodaj_posjednika = operat.dodaj_posjednika(posjednik)
+    return render_template('dodaj_posjednika.html')
+
+@app.route('/azuriraj_posjednika/', methods = ['GET', 'POST'])
+def azuriraj_posjednika():
+    jmbg = request.form.get('jmbg')
+    ime = request.form.get('ime')
+    prezime = request.form.get('prezime')
+    vrsta_prava = request.form.get('vrstaprava')
+    obim_prava = request.form.get('obimprava')
+    azuriraj = operat.azuriraj_posjednika(ime, prezime, vrsta_prava, obim_prava, jmbg)
+    return render_template('azuriraj_posjednika.html')
+
+
+@app.route('/izbrisi_posjednika/', methods = ['GET', 'POST'])
+def izbrisi_posjednika():
+    jmbg = request.form.get('jmbg')
+    izbrisi = operat.izbrisi_posjednika(jmbg)
+    return render_template('izbrisi_posjednika.html')
+
+
+
+@app.route('/pretrazi_posjednika/', methods = ['GET', 'POST'])
+def pretraga_pos():
     if request.method == 'POST':
+        cur = mysql.connection.cursor()
         jmbg = request.form.get('jmbg')
+        preuzmi_posjednika = cur.execute("SELECT * FROM POSJEDNIK WHERE jmbg = %s", [jmbg])
+        if preuzmi_posjednika > 0:
+            pretraga_posjednika = cur.fetchall()
+            cur.execute("SELECT POS_LIST_id_pl from POSJEDNIK WHERE jmbg = %s", [jmbg])
+            br_id = cur.fetchall()
+            br_pl = operat.br_pl_id_pl(br_id)
+            flask.flash('Uspjesna pretraga!', 'success')
+            return render_template('pretrazi_posjednika.html', rezultat = pretraga_posjednika, br_pl = br_pl)
+        else:
+            flask.flash('Ne postoji JMBG u bazi!', 'danger')
+            return render_template('pretrazi_posjednika.html')
+    else:
+        return render_template('pretrazi_posjednika.html')
+
+@app.route('/pretrazi_pos/', methods = ['GET', 'POST'])   
+def pretraga_pos1():
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
         ime = request.form.get('ime')
         prezime = request.form.get('prezime')
-        vrsta_prava = request.form.get('vrstaprava')
-        obim_prava = request.form.get('obimprava')
-        pos_list =request.form.get('brpl')
-        id_pl_preuzet = operat.br_id_pl(pos_list)
-        posjednik = operat.Posjednici(jmbg, ime, prezime, vrsta_prava, obim_prava, id_pl_preuzet)
-        dodaj_posjednika = operat.Posjednici.dodaj_posjednika(posjednik)
-    return render_template('dodaj_posjednika.html')
-    
+        preuzmi_posjednika = cur.execute("SELECT * FROM  POSJEDNIK WHERE ime = %s AND prezime = %s", [ime, prezime])
+        if preuzmi_posjednika > 0:
+            pretraga_posjednika = cur.fetchall()
+            cur.execute("SELECT POS_LIST_id_pl from POSJEDNIK WHERE ime = %s AND prezime =%s", [ime, prezime])
+            br_id = cur.fetchall()
+            br_pl = operat.br_pl_id_pl(br_id)
+            flask.flash('Uspjesna pretraga!', 'success')
+            return render_template('pretrazi_posjednika.html',rezultat1 = pretraga_posjednika, br_pl = br_pl)
+        else:
+            flask.flash('Ne postoji korisnik u bazi!', 'danger')
+            return render_template('pretrazi_posjednika.html')
+    else:
+        return render_template('pretrazi_posjednika.html')
+
+
+######################################PARCELE###############################################################################
         
 @app.route('/dodaj_parcele/', methods = ['GET', 'POST'])
-def dodaj_parcele():
-    if request.method == 'POST':       
-        id_parcele = uuid.uuid4().int
-        id_parcele = str(id_parcele)
-        br_parcele = request.form.get('brparcele')
-        pbr_parcele = request.form.get('pbrparcele')
-        kultura = request.form.get('kultura')
-        klasa = request.form.get('klasa')
-        povrsina = request.form.get('povrsina')
-        plan = request.form.get('plan')
-        skica = request.form.get('skica')
-        broj_pl = request.form.get('brojpl')
-        parcela = operat.Parcele(id_parcele, br_parcele, pbr_parcele, kultura, klasa, povrsina, plan, skica, broj_pl)
-        dodaj_parcelu = operat.dodaj_parcelu(parcela)
+def dodaj_parcele():    
+    id_parcele = int(uuid.uuid4())
+    br_parcele = request.form.get('brparcele')
+    pbr_parcele = request.form.get('pbrparcele')
+    kultura = request.form.get('kultura')
+    klasa = request.form.get('klasa')
+    povrsina = request.form.get('povrsina')
+    plan = request.form.get('plan')
+    skica = request.form.get('skica')
+    broj_pl = request.form.get('brojpl')
+    id_pl = operat.br_id_pl(broj_pl)
+    parcela = operat.Parcele(id_parcele, br_parcele, pbr_parcele, kultura, klasa, povrsina, plan, skica, broj_pl, id_pl)
+    dodaj_parcelu = operat.dodaj_parcelu(parcela)
     return render_template('dodaj_parcele.html')
 
+@app.route('/azuriraj_parcele/', methods = ['GET', 'POST'])
+def azuriraj_parcele():
+    br_parcele = request.form.get('br_parcele')
+    pbr_parcele = request.form.get('pbr_parcele')
+    kultura = request.form.get('kultura')
+    klasa = request.form.get('klasa')
+    povrsina = request.form.get('povrsina')
+    plan = request.form.get('plan')
+    skica = request.form.get('skica')
+    br_parcele1 = request.form.get('br_parcele1')
+    pbr_parcele1 = request.form.get('pbr_parcele1')
+    azuriraj = operat.azuriraj_parcelu(br_parcele, pbr_parcele, kultura, klasa, povrsina, plan, skica, br_parcele1, pbr_parcele1)
+    return render_template('azuriraj_parcele.html')
+
+@app.route('/izbrisi_parcele/', methods = ['GET', 'POST'])
+def izbrisi_parcele():
+    br_parcele = request.form.get('brparcele')
+    pbr_parcele = request.form.get('pbrparcele')
+    izbrisi = operat.izbrisi_parcelu(br_parcele, pbr_parcele)
+    return render_template('izbrisi_parcele.html')
+
+@app.route('/pretrazi_parcele/', methods = ['GET', 'POST'])
+def pretraga():
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        br_parcele = request.form.get('brparcele')
+        if br_parcele.isdigit():
+            preuzmi = cur.execute("SELECT * FROM PARCELE WHERE br_parcele = %s", [br_parcele])
+            if preuzmi > 0:
+                pretraga = cur.fetchall()
+                flask.flash('Uspjesna pretraga!', 'success')
+                return render_template('pretrazi_parcele.html', rezultat=pretraga)
+            else:
+                flask.flash('Ne postoji parcela u bazi!', 'danger')
+                return render_template('pretrazi_parcele.html')
+        else:
+            flask.flash('Unos mora biti broj!', 'danger')
+            return render_template('pretrazi_parcele.html')
+    else:
+        return render_template('pretrazi_parcele.html')
 
 
+######################################OSTALO###############################################################################
+@app.route('/eksport_parcela')
+def eksport_parcela():
+    eksport = operat.export_parcela()
+    return render_template('kat_operat.html')
 
+@app.route('/eksport_posjednika')
+def eksport_posjednika():
+    eksport = operat.export_posjednika()
+    return render_template('kat_operat.html')
 
-
+@app.errorhandler(404)
+def invalid_route(e):
+    return render_template("404.html")
 
 
         
@@ -156,3 +295,4 @@ def dodaj_parcele():
         
         
 
+        
--- operat.py
+++ operat.py
@@ -4,6 +4,7 @@ from flask_mysqldb import MySQL
 import flask
 import yaml
 import uuid
+import csv
 
 app = Flask(__name__)
 
@@ -16,9 +17,9 @@ app.config['MYSQL_DB'] = db['mysql_db']
 mysql = MySQL(app)
 
 
-
+######################################PARCELE###############################################################################
 class Parcele():
-    def __init__(self, id_parcele, br_parcele, pbr_parcele, kultura, klasa, pov, plan, skica, broj_pl):
+    def __init__(self, id_parcele, br_parcele, pbr_parcele, kultura, klasa, pov, plan, skica, broj_pl, id_pl):
         self.id_parcele = id_parcele
         self.br_parcele = br_parcele
         self.pbr_parcele = pbr_parcele
@@ -28,33 +29,78 @@ class Parcele():
         self.plan = plan
         self.skica = skica
         self.broj_pl = broj_pl
+        self.id_pl = id_pl
+
+def dodaj_parcelu(parcela):
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        pretraga_parcele = cur.execute("SELECT * FROM PARCELE WHERE br_parcele = %s AND pbr_parcele = %s", [parcela.br_parcele, parcela.pbr_parcele])
+        if cur.fetchone() is None:
+            unos = "INSERT INTO PARCELE (idPARCELE, br_parcele, pbr_parcele, kultura, klasa, povrsina, plan, skica, broj_pl, POS_LIST_id_pl) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            parametri = (parcela.id_parcele, parcela.br_parcele, parcela.pbr_parcele, parcela.kultura, parcela.klasa, parcela.pov, parcela.plan, parcela.skica, parcela.broj_pl, parcela.id_pl)
+            cur.execute(unos,parametri)
+            mysql.connection.commit()
+            cur.close()
+            flask.flash('Uspjesan unos!', 'succes')
+        else:
+            flask.flash('Parcela vec postoji!', 'danger')
+            redirect(url_for('dodaj_parcele')) 
+    return render_template('dodaj_parcele.html')  
+        
+def azuriraj_parcelu(br_parcele, pbr_parcele, kultura, klasa, povrsina, plan, skica, br_parcele1, pbr_parcele1):
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        azuriraj = cur.execute("UPDATE PARCELE SET br_parcele = %s, pbr_parcele = %s, kultura = %s, klasa = %s, povrsina = %s, plan = %s, skica = %s WHERE br_parcele = %s AND pbr_parcele = %s ")
+        parametri = (br_parcele, pbr_parcele, kultura, klasa, povrsina, plan, skica, br_parcele1, pbr_parcele1)
+        cur.execute(azuriraj, parametri)
+        mysql.connection.commit()
+        cur.close()
+        flask.flash('Uspješno ažurirani podaci!', 'succes')
+    return render_template('azuriraj_parcele.html')
+
+def izbrisi_parcelu(br_parcele, pbr_parcele):
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        izbrisi = "DELETE FROM PARCELE WHERE br_parcele = %s AND pbr_parcele =%s"
+        parametri = (br_parcele, pbr_parcele)
+        cur.execute(izbrisi, parametri)
+        mysql.connection.commit()
+        cur.close()
+        flask.flash('Podaci izbrisani!', 'success')
+    return render_template('izbrisi_parcele.html')
 
 
+
+    
+######################################POSJEDOVNI LIST###############################################################################
 class PosList():
     def __init__(self, id_pl, broj_pl):
         self.id_pl = id_pl
         self.broj_pl = broj_pl
-    def dodaj_pl(pl):
-        if request.method == 'POST':
-            cur = mysql.connection.cursor()
-            cur.execute("SELECT * from POS_LIST WHERE broj_pl = %s ", [pl.broj_pl])
-            if cur.fetchone() is None:
-                unos = "INSERT INTO POS_LIST (id_pl, broj_pl) VALUES(%s, %s)"
-                parametri = (pl.id_pl, pl.broj_pl)
-                if pl.broj_pl.isdigit():
-                    cur.execute(unos,parametri)
-                    mysql.connection.commit()
-                    flask.flash('Uspjesan unos PL/LN!', 'succes')
-                    redirect(url_for('dodaj_posjednika'))  
-                else:
-                     flask.flash('Unos mora biti broj!', 'danger')
-                     redirect(url_for('dodaj_pl'))  
+        
+def dodaj_pl(pl):
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * from POS_LIST WHERE broj_pl = %s ", [pl.broj_pl])
+        if cur.fetchone() is None:
+            unos = "INSERT INTO POS_LIST (id_pl, broj_pl) VALUES(%s, %s)"
+            parametri = (pl.id_pl, pl.broj_pl)
+            if pl.broj_pl.isdigit():
+                cur.execute(unos,parametri)
+                mysql.connection.commit()
+                flask.flash('Uspjesan unos PL/LN!', 'success')
+                redirect(url_for('dodaj_posjednika'))
+                render_template('dodaj_posjednika.html') 
             else:
-                flask.flash('PL/LN vec postoji!', 'danger')
-                redirect(url_for('dodaj_pl'))   
+                 flask.flash('Unos mora biti broj!', 'danger')
+                 redirect(url_for('dodaj_pl'))  
         else:
-            render_template('dodaj_posjednika.html')
+            flask.flash('PL/LN vec postoji!', 'danger')
+            redirect(url_for('dodaj_pl'))   
+    else:
+        render_template('dodaj_pl.html')
 
+######################################POSJEDNICI###############################################################################
 class Posjednici():
     def __init__(self, jmbg, ime, prezime, vrsta_prava, obim_prava, broj_pl):
         self.jmbg = jmbg
@@ -64,57 +110,86 @@ class Posjednici():
         self.obim_prava = obim_prava
         self.broj_pl = broj_pl
     
-    def dodaj_posjednika(posjednik):
-        if request.method == 'POST':
-            cur = mysql.connection.cursor()
-            cur.execute("SELECT * from POSJEDNIK WHERE jmbg = %s", [posjednik.jmbg])
-            if cur.fetchone() is None:
-                unos = "INSERT INTO POSJEDNIK (jmbg, ime, prezime, vrsta_prava, obim_prava, POS_LIST_id_pl) VALUES(%s, %s,%s,%s,%s,%s)"
-                parametri = (posjednik.jmbg, posjednik.ime, posjednik.prezime, posjednik.vrsta_prava, posjednik.obim_prava, posjednik.broj_pl)
-                cur.execute(unos,parametri)
-                mysql.connection.commit()
-                flask.flash('Uspjesan unos posjednika!', 'succes')
-            else:
-                flask.flash('Posjednik vec postoji!', 'danger')
-                redirect(url_for('dodaj_posjednika'))
-        else:
-            render_template('dodaj_posjednika.html')
-
-def br_id_pl(broj_pl):
-    cur= mysql.connection.cursor()
-    cur.execute("SELECT id_pl FROM POS_LIST WHERE broj_pl = %s", [broj_pl])
-    rezultat = cur.fetchone()
-    return rezultat
-
-
-
-#NAPRAVITI DA SE POSTAVLJA UPIT< KOLIKO POSJEDNIKA ZELITE UNIJETI U POSJEDOVNI LIST
-            
-
-
-def dodaj_parcelu(parcela):
+def dodaj_posjednika(posjednik):
     if request.method == 'POST':
         cur = mysql.connection.cursor()
-        unos = "INSERT INTO PARCELE (idPARCELE, br_parcele, pbr_parcele, kultura, klasa, povrsina, plan, skica, broj_pl, POS_LIST_id_pl) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        parametri = (parcela.id_parcele, parcela.br_parcele, parcela.pbr_parcele, parcela.kultura, parcela.pov, parcela.plan, parcela.skica, parcela.broj_pl)
-        cur.execute(unos,parametri)
+        cur.execute("SELECT * from POSJEDNIK WHERE jmbg = %s", [posjednik.jmbg])
+        if cur.fetchone() is None:
+            unos = "INSERT INTO POSJEDNIK (jmbg, ime, prezime, vrsta_prava, obim_prava, POS_LIST_id_pl) VALUES(%s, %s,%s,%s,%s,%s)"
+            parametri = (posjednik.jmbg, posjednik.ime, posjednik.prezime, posjednik.vrsta_prava, posjednik.obim_prava, posjednik.broj_pl)
+            cur.execute(unos,parametri)
+            mysql.connection.commit()
+            flask.flash('Uspjesan unos posjednika!', 'succes')
+        else:
+            flask.flash('Posjednik vec postoji!', 'danger')
+            redirect(url_for('dodaj_posjednika'))
+    else:
+        render_template('dodaj_posjednika.html')
+
+
+def azuriraj_posjednika(ime, prezime, vrsta_prava, obim_prava, jmbg):
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        azuriraj = "UPDATE POSJEDNIK SET ime = %s, prezime = %s, vrsta_prava = %s, obim_prava = %s WHERE jmbg = %s"
+        parametri = (ime, prezime, vrsta_prava, obim_prava, jmbg)
+        cur.execute (azuriraj, parametri)
         mysql.connection.commit()
         cur.close()
         flask.flash('Uspjesan unos!', 'succes')
-        redirect(url_for('dodaj_parcele'))
     else:
-        render_template('dodaj_parcele.html')  
+        render_template('azuriraj_posjednika.html')
+        
+def izbrisi_posjednika(jmbg):
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        izbrisi = "DELETE FROM POSJEDNIK WHERE jmbg = %s" 
+        parametri = (jmbg,)
+        cur.execute(izbrisi, parametri)
+        mysql.connection.commit()
+        cur.close()
+        flask.flash('Posjednik izbrisan!', 'succes')
+    else:
+        render_template('izbrisi_posjednika.html')
 
-def parcela_pl(br_pl):
+
+
+ ######################################OSTALO###############################################################################   
+
+def br_id_pl(broj_pl):
+    cur= mysql.connection.cursor()
+    preuzmi_id_pl = cur.execute("SELECT id_pl FROM POS_LIST WHERE broj_pl = %s", [broj_pl])
+    rezultat = cur.fetchall()
+    return rezultat
+
+def br_pl_id_pl(id_pl):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT FROM POS_LIST id_pl AND id_ko WHERE broj_pl = ?", (br_pl))
-    separator = ","
-    result = separator.join(cur.fetchone())
-    return result
-    
+    preuzmi_pl = cur.execute("SELECT broj_pl FROM POS_LIST WHERE id_pl = %s", [id_pl])
+    rezultat = cur.fetchall()
+    return rezultat      
 
+def export_parcela():
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        parcele = cur.execute("SELECT * FROM PARCELE")
+        with open('parcele.csv', 'w', newline='') as parcele:
+            csv_parcele = csv.writer(parcele)
+            csv_parcele.writerow([i[0] for i in cur.description])
+            csv_parcele.writerows(cur)
+        flask.flash('Uspješan izvoz parcela!', 'succes')
+    else:
+        return render_template('kat_operat.html')
 
-
+def export_posjednika():
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        posjednici = cur.execute("SELECT * FROM POSJEDNIK")
+        with open('posjednici.csv', 'w', newline='') as posjednici:
+            csv_posjednici = csv.writer(posjednici)
+            csv_posjednici.writerow([i[0] for i in cur.description])
+            csv_posjednici.writerows(cur)
+        flask.flash('Uspješan izvoz posjednika!', 'succes')
+    else:
+        return render_template('kat_operat.html')
 
 
 
